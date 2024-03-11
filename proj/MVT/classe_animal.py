4e581919e047e76b44eeb1aa9312651458f16478
GIGANT Tom <135255682+JustRhynd@users.noreply.github.com>
2024-03-11 13:28:03 +0000

    .

--- proj/MVT/classe_animal.py
+++ proj/MVT/classe_animal.py
@@ -6,10 +6,10 @@ class Animal:
     Attribut de classe :
         v_max : norme maximale du vecteur vitesse
         v_init : norme du vecteur vitesse lors de la création de l'objet
-        force_max = force maximale s'exerçant sur l'animal. Pour un comportement réaliste, 
+        force_max = force maximale s'exerçant sur l'animal. Pour un comportement réaliste,
                     un animal ne peut pas par exemple faire un demi-tour immédiatement
     Attributs :
-        
+
         position :      vecteur de coordonnées x, y aléatoires, dans les limites de la fenêtre d'affichage
         vitesse :       vecteur  sous la forme d'une liste de flottants
         taille :        rayon en pixels de l'animal. C'est comme en physique, tout est
@@ -18,18 +18,18 @@ class Animal:
             perçoit pas ce qui se passe derrière lui. On suppose qu'il a une
             vision à 300 degrés, soit +/- 150 degrés par rapport à sa direction.
             La liste comprend 3 éléments qui correspondent aux trois règles de déplacement.
-            
+
     Méthodes :
-        force_alea : crée une force de déplacement aléatoire qui va 
+        force_alea : crée une force de déplacement aléatoire qui va
                     s'appliquer sur l'animal
         maj_position : déplace l'animal suivant sa vitesse.
         distance : revoie la distance avec un autre Animal
-        
+
     """
     v_max = 4
     v_init = 2
-    force_max = 0.5
-    
+    force_max = 1
+
     def __init__(self, l_univers, h_univers):
         self.taille = 2
         self.l_univers = l_univers
@@ -46,7 +46,7 @@ class Animal:
         self.vitesse.prodk(self.v_init/self.vitesse.norme()) # on met la norme de la vitesse à v_init
         self.perception = [30, 100, 200]     # proche, moyen, distant
         self.force = Vecteur(0, 0)
-        
+
     def force_alea(self):
         self.force.x = random() - 0.5
         self.force.y = random() - 0.5
@@ -55,9 +55,9 @@ class Animal:
             self.force.prodk(self.force_max/self.force.norme())
 
         return self.force
-    
+
     def maj_position(self):
-        self.force_alea()            # test avec une force aléatoire (question 2b) 
+        #self.force_alea()            # test avec une force aléatoire (question 2b)
         self.position.x += self.vitesse.x
         self.position.y += self.vitesse.y
         if self.position.x < 0 or self.position.x > self.l_univers :
@@ -68,13 +68,13 @@ class Animal:
         self.vitesse.y += self.force.y
         if self.vitesse.norme() > self.v_max :
             self.vitesse.prodk(self.v_max/self.vitesse.norme())
-        
 
+        #self.force_alea()
         return self.position
-   
+
     def distance(self, autre):
         return sqrt((autre.position.x - self.position.x)**2 + (autre.position.y - self.position.y)**2)
-    
+
     def __repr__(self):
         chaine = "Position : (" + str(self.position.x) + " , " + str(self.position.y) + ")\n"
         chaine += "Vitesse : (" + str(self.vitesse.x)  + " , " + str(self.vitesse.y) + ")\n"
